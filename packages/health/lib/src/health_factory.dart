114d921f9f76f04d7513fcb1f0cc2bf8590f8cd4
Adam Oakman <adam.oakman@gmail.com>
2021-02-03 13:28:08 +0000

    Add source information to HealthDataPoint
    Added two new fields to the HealthDataPoint, SourceId and SourceName and populate when data is read. This allows datapoints to be disambigous and in some cases allows us to get more accurate data. For example the number of steps can be reported from Apple Health and Watch and without source data they are aggregated into just "steps" producing an innacurate result.
    
    On iOS source is derived from sample.sourceRevision.source (https://developer.apple.com/documentation/healthkit/hksourcerevision) with id coming from bundleIdentifier ("com.apple.health") and name from name ("Health")
    
    On Android source is derived from dataPoint.getOriginalDataSource() (https://developers.google.com/android/reference/com/google/android/gms/fitness/data/DataPoint#getOriginalDataSource())with id from getStreamIdentifier and name getAppPackageName
    
     Conflicts:
    	packages/health/lib/src/health_factory.dart

--- packages/health/lib/src/health_factory.dart
+++ packages/health/lib/src/health_factory.dart
@@ -62,7 +62,9 @@ class HealthFactory {
           weights[i].dateFrom,
           weights[i].dateTo,
           _platformType,
-          _deviceId);
+          _deviceId,
+          null, // TODO fix source ID and name
+          null);
 
       bmiHealthPoints.add(x);
     }
@@ -121,15 +123,15 @@ class HealthFactory {
 
     try {
       List fetchedDataPoints = await _channel.invokeMethod('getData', args);
-      if (fetchedDataPoints != null) {
-        healthData = fetchedDataPoints.map((e) {
-          num value = e["value"];
-          DateTime from = DateTime.fromMillisecondsSinceEpoch(e["date_from"]);
-          DateTime to = DateTime.fromMillisecondsSinceEpoch(e["date_to"]);
-          return HealthDataPoint._(
-              value, dataType, unit, from, to, _platformType, _deviceId);
-        }).toList();
-      }
+      healthData = fetchedDataPoints.map((e) {
+        num value = e["value"];
+        DateTime from = DateTime.fromMillisecondsSinceEpoch(e["date_from"]);
+        DateTime to = DateTime.fromMillisecondsSinceEpoch(e["date_to"]);
+        String sourceId = e["source_id"];
+        String sourceName = e["source_name"];
+        return HealthDataPoint._(value, dataType, unit, from, to, _platformType,
+            _deviceId, sourceId, sourceName);
+      }).toList();
     } catch (error) {
       print("Health Plugin Error:\n");
       print("\t$error");
